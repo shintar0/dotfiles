eb0bdd139ebd5061b6d403b3b33e5fab3f4eb2e1
shintar0 <133033959+shintar0@users.noreply.github.com>
2026-03-28 00:22:04 +0900

    🚀 setup: slを自動で張るためのsetupスクリプトを追加

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# シンボリックリンクを作成するファイルとパスのリスト
+files_and_paths=(
+  ".bashrc:~/.bashrc"
+)
+
+# シンボリックリンクを作成する関数
+create_symlink() {
+  local source_file=$(realpath $1)
+  local destination_path=$2
+
+  # 退避先のファイル名
+  backup_file="${destination_path}.bak"
+
+  # バックアップファイルが存在する場合は削除
+  if [ -e "$destination_path" ]; then
+    # バックアップ
+    mv "$destination_path" "$backup_file"
+  fi
+
+  # シンボリックリンクの作成
+  ln -s "$source_file" "$destination_path"
+}
+
+# ファイルとパスのリストをループしてシンボリックリンクを作成
+for entry in "${files_and_paths[@]}"; do
+  IFS=":" read -r source_file destination_path <<< "$entry"
+  create_symlink "$source_file" "$destination_path"
+done
